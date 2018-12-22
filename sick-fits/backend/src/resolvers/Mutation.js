766316f906e3bdf528d524bbfd7155a6e24b5669
Guga Gongadze <ggong13@freeuni.edu.ge>
2018-12-23 00:58:34 +0400

    Feat: add relationship between items and users

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -6,10 +6,19 @@ const { transport, makeANiceEmail } = require('../mail')
 
 const Mutations = {
   async createItem(parent, args, ctx, info) {
-    // TODO: Chech if they are logged in
+    if (!ctx.request.userId) {
+      throw new Error('You must be logged in to do that!')
+    }
 
     const item = await ctx.db.mutation.createItem({
-      data: { ...args }
+      data: {
+        user: {
+          connect: {
+            id: ctx.request.userId
+          }
+        },
+        ...args
+      }
     }, info)
 
     return item
